dd0ca6265f390694bd7a8c7fdb5da88d74db51c3
amrit <amritpals90@gmail.com>
2024-05-30 23:55:54 +0100

    Base for REST API

---
+++ src/test/java/com/brand/resume_manager/ResumeManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brand.resume_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
